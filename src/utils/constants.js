5a104f03b160cc80107c0ab1f99fea0008a08edd
parameshvaran <123230338+karthi-hash@users.noreply.github.com>
2025-05-09 20:27:57 +0530

    Delete src directory

--- src/utils/constants.js
+++
@@ -1,6 +0,0 @@
- export const CDN_URL=
- "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
- export const MENU_API=
- "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=";
- 
